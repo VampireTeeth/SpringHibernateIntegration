fa49730c71b4ed3b3ad37ad625a152394d18cd2c
Emagine <emagine@greglaptop.emagineinternational.com>
2016-01-19 17:43:54 +1100

    jsp and web.xml changes

--- src/main/java/org/vampireteeth/springhibernate/controller/EmployeeController.java
+++ src/main/java/org/vampireteeth/springhibernate/controller/EmployeeController.java
@@ -3,6 +3,9 @@ package org.vampireteeth.springhibernate.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.vampireteeth.springhibernate.entity.EmployeeEntity;
@@ -20,6 +23,19 @@ public class EmployeeController {
 		map.addAttribute("employee", new EmployeeEntity());
 		map.addAttribute("employeeList", employeeService.getAllEmployees());
 		return "editEmployeeList";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addEmployee(@ModelAttribute(value="employee") EmployeeEntity employee, 
+			BindingResult result) {
+		this.employeeService.addEmployee(employee);
+		return "redirect:/";
+	}
+
+	@RequestMapping(value = "/delete/{employeeId}", method = RequestMethod.GET)
+	public String deleteEmployee(@PathVariable("employeeId") Integer employeeId) {
+		this.employeeService.deleteEmployee(employeeId);
+		return "redirect:/";
 	}
 
 	public void setEmployeeService(EmployeeService employeeService) {
